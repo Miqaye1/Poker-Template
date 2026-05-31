b69704422991587d6cc582a4e2aaee1eefd97bb2
Miqaye1 <mikayelmnatsakanyan@gmail.com>
2026-05-31 16:33:58 +0200

    Merge branch 'main' of https://github.com/Miqaye1/Poker-Template

--- player.py
+++ player.py
@@ -10,25 +10,42 @@ from cards import Card
 @dataclass
 class Player:
     name: str
+    chips : int = 1000
+    is_human : bool = True
+    hole_cards: list[Card] = field(default_factory=list)
+    current_bet: int = 0
+    folded: bool = False
     # TODO: Task 1 - add chips, is_human, hole_cards, current_bet, and folded
 
     def reset_for_hand(self) -> None:
+        self.hole_cards = []
+        self.current_bet = 0
+        self.folded = False
         # TODO: Task 2 - reset the player's state for a new hand
-        pass
 
     def receive(self, cards: list[Card]) -> None:
+        for i in cards:
+            self.hole_cards.append(i)
         # TODO: Task 3 - add the received cards to the player's hole cards
         pass
 
     def bet(self, amount: int) -> int:
+        if amount < 0:
+            raise ValueError("Bet amount cannot be negative")
+        if self.chips >= amount:
+            self.chips = self.chips - amount
+            self.current_bet = self.current_bet + amount
+            return amount
+        else:
+            raise ValueError("Not enough chips to place this bet")
         # TODO: Task 4 - check if the player has enough chips to bet the specified amount, 
         # and if so, deduct the amount from the player's chips and add it to the current bet
-        if amount < 0:
-            pass
-        pass 
+        
+        
 
     @property
     def active(self) -> bool:
+        #return not self.folded
+        return self.chips > 0
         # TODO: Task 5 - return True if the player is still active in the hand
-        pass
 
--- table.py
+++ table.py
@@ -11,14 +11,13 @@ from player import Player
 # TODO: Task 1 - implement the Table class 
 class Table:
     def __init__(self, table_cards: list, players: list[Player], pot: int):
-        self.table_cards = players
+        self.table_cards = []
         self.players = players
-        self.pot = pot
+        self.pot = 0
     def reset(self) -> None:
         self.table_cards = []
         self.pot = 0
-    def add_to_pot(self):
-        for player in self.players:
-            self.pot += player.current_bet
+    def add_to_pot(self, amount: int):
+        self.pot += amount
     # TODO: Task 2 - create and implement the method reset(self) -> None 
     # TODO: Task 3 - create and implement the method add_to_pot(self, amount: int) -> None
